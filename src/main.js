ce5b28a5554ef3d804859e9325f2725b369d1467
Konstantin Surikov <surikov@gmail.com>
2019-08-22 11:56:02 +0300

    Личный проект: и ехать и шашечки

--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@ import {getProfileTemplate} from './components/profile';
 import {getNavigationTemplate} from './components/navigation';
 import {getSortTemplate} from './components/sorting';
 import {getFilmsTemplate} from './components/films';
-import {getFilmCardTemplate} from './components/film-card';
+import {getFilmCardsTemplate} from './components/film-cards';
 import {getShowMoreTemplate} from './components/show-more';
+import {user, getMovies, getComments} from './data';
+
+export const CARDS_PER_CLICK = 5;
+export const state = {
+  cardsDisplayed: 0
+};
+
+const movies = getMovies();
+const comments = getComments();
 
 const header = document.querySelector(`.header`);
 const main = document.querySelector(`.main`);
@@ -13,29 +22,25 @@ const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
 };
 
-
 render(header, getSearchTemplate());
-render(header, getProfileTemplate());
-render(main, getNavigationTemplate());
+render(header, getProfileTemplate(user));
+render(main, getNavigationTemplate(movies));
 render(main, getSortTemplate());
-render(main, getFilmsTemplate());
+render(main, getFilmsTemplate(movies, comments));
 
 const films = document.querySelector(`.films`);
 const filmsList = films.querySelector(`.films-list`);
 const filmsListContainer = films.querySelector(`.films-list__container`);
-for (let i = 0; i < 5; i++) {
-  render(filmsListContainer, getFilmCardTemplate());
-}
+
+render(filmsListContainer, getFilmCardsTemplate(movies, comments));
+
+
 render(filmsList, getShowMoreTemplate());
+const showMoreButton = document.querySelector(`.films-list__show-more`);
 
-const filmsListExtra = films.querySelectorAll(`.films-list--extra`);
-const [topRatedList, mostCommentedList] = filmsListExtra;
-const topRatedListContainer = topRatedList.querySelector(`.films-list__container`);
-const mostCommentedListContainer = mostCommentedList.querySelector(`.films-list__container`);
-for (let i = 0; i < 2; i++) {
-  render(topRatedListContainer, getFilmCardTemplate());
-}
-for (let i = 0; i < 2; i++) {
-  render(mostCommentedListContainer, getFilmCardTemplate());
-}
-
+showMoreButton.addEventListener(`click`, () => {
+  render(filmsListContainer, getFilmCardsTemplate(movies, comments));
+  if (state.cardsDisplayed >= movies.length) {
+    filmsList.removeChild(showMoreButton);
+  }
+});
